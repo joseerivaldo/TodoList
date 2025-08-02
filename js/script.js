0a8e4ec6934db965ab37d4e68a54423095ea423e
José Erivaldo Meneses <67519141+joseerivaldo@users.noreply.github.com>
2025-08-02 06:38:36 -0300

    js att

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ const saveTodo = (text) =>{
     todoList.appendChild(todo)
 
     todoInput.value = ''
+    todoInput.focus()
 }
 
 //Eventos
@@ -46,3 +47,17 @@ todoForm.addEventListener("submit", (e) => {
     }
 })
 
+document.addEventListener('click', (e)=>{
+    const targetEl = e.target
+    const parentEl = targetEl.closest('div')
+
+    if(targetEl.classList.contains('finish-todo')){
+        console.log('Clicou em finalizar')
+    }
+    else if(targetEl.classList.contains('edit-todo')){
+        console.log('Clicou em editar')
+    }
+    else if(targetEl.classList.contains('remove-todo')){
+        console.log('Clicou em remover')
+    }
+})
